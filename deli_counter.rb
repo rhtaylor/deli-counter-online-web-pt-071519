f22505100edf1f95f8c01fde359905d07e07fc03
Randall Taylor <randyisbusy@gmail.com>
2019-07-18 16:59:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+katz_deli = []
+
+def line(deli=katz_deli)
+    if deli.size < 1
+      puts "The line is currently empty."
+    else
+      nu = []
+     deli.each_with_index { |x,i| nu << "#{i+1}. #{x}"}
+      sen = "The line is currently: "
+      nu.each {|x| sen << x }
+      puts sen
+    end
+
+end
+
+
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  katz_deli.each_with_index do |n, i|
+    puts  "Welcome, #{n}. You are number #{i + 1}"
+  end
+end
