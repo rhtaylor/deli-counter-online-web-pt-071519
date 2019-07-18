455ea7bb25b103611b9bccf47ced657ec69ec831
Randall Taylor <randyisbusy@gmail.com>
2019-07-18 17:25:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(deli=katz_deli)
       nu = []
      deli.each_with_index { |x,i| nu << "#{i+1}. #{x}"}
       sen = "The line is currently: "
-      nu.each do 
+      nu.each do
         |x| sen.join(" #{x}")
       end
       puts sen
