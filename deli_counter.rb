c668cea99a8e46cd8638c265f51533e14a8b7d63
Randall Taylor <randyisbusy@gmail.com>
2019-07-18 17:17:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,6 +23,9 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(deli)
+  if deli.size <= 0
+    puts "There is nobody waiting to be served!"
+  else
   spot = deli.shift
   puts "Currently serving #{spot}."
 end
